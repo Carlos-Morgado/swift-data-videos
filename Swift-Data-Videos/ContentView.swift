51aa2e747c14af807bd959df1d9330f205da2902
Carlos Morgado <cmorgadoalarcon@gmail.com>
2024-04-16 17:12:00 +0200

    Creating project

---
+++ Swift-Data-Videos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-Data-Videos
+//
+//  Created by Carlos Morgado on 16/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-Data-Videos/Swift_Data_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Data_VideosApp.swift
+//  Swift-Data-Videos
+//
+//  Created by Carlos Morgado on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Data_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
